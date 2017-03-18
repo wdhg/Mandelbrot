f52dda75bfc26f32329d6df8534a79d68eb8fa2d
wdhg <wdhgrant@gmail.com>
2017-03-18 15:11:10 +0000

    First commit

---
+++ Mandelbrot/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Mandelbrot {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Output = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Output)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Output.Location = new System.Drawing.Point(0, 0);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(484, 461);
+            this.Output.TabIndex = 0;
+            this.Output.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.Text = "Mandelbrot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Output)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Output;
+    }
+}
+
---
+++ Mandelbrot/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mandelbrot {
+    public partial class Form1 : Form {
+
+        private const int FractalSize = 4; // Graph units, from negative to position (-2 to 2)
+        private const int ImageSize = 800; // Pixels
+        private const float HalfImageSize = ImageSize / 2; // For quick and easy centring
+        private const float ImageFractalRatio = (float) FractalSize / ImageSize; // How big each pixel is on the graph
+        private const int Limit = 2; // Used for checking if the value is in the sequence
+        private const int Iterations = 20; // The larger the value, the more accurate
+        private const float XShift = -1; // To shift the graph over to the right so it's centred
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Bitmap fractalImage = new Bitmap(ImageSize, ImageSize);
+
+            for (int x = 0; x < ImageSize; x++) {
+                for (int y = 0; y < ImageSize; y++) {
+                    if (InMandelbrot(new Complex((x - HalfImageSize) * ImageFractalRatio + XShift, (y - HalfImageSize) * ImageFractalRatio), Iterations)) {
+                        fractalImage.SetPixel(x, y, Color.Black);
+                    } else {
+                        fractalImage.SetPixel(x, y, Color.White);
+                    }
+                }
+            }
+
+            Output.Image = fractalImage;
+        }
+
+        // Zn+1 = Zn ^ 2 + C
+        private bool InMandelbrot(Complex C, int iterations) {
+            Complex Z = new Complex();
+
+            for (int i = 0; i < iterations; i++) {
+                if (Z.Abs() > 2) {
+                    return false;
+                } else {
+                    Z = Z * Z + C;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class Complex {
+        public float x, y;
+
+        public Complex(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Complex() {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public override string ToString() {
+            return string.Format("({0} + {1}i)", this.x, this.y);
+        }
+
+        public double Abs() {
+            return Math.Sqrt(
+                Math.Pow(this.x, 2) +
+                Math.Pow(this.y, 2)
+            );
+        }
+
+        public static Complex operator +(Complex a, Complex b) {
+            return new Complex(a.x + b.x, a.y + b.y);
+        }
+
+        public static Complex operator *(Complex a, Complex b) {
+            return new Complex(
+                a.x * b.x + (a.y * b.y * -1), // Because i^2 is -1
+                (a.x * b.y) + (a.y * b.x)
+            );
+        }
+    }
+}
